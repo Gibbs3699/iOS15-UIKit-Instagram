6a29acce4eaf9e9e9ed719fba7ce0ec77128bec8
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-10-17 23:44:29 +0700

    init project

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by TheGIZzz on 17/10/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
