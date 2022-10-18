457249a880dbaceaddde0f365130cfd9900b1476
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-10-18 22:24:24 +0700

    feat: install firebase with cocopods and delete storyboard

---
+++ Instagram/Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  Instagram
+//
+//  Created by TheGIZzz on 17/10/2565 BE.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+
+
+}
+
